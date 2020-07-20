d79eba9ddef6d3987f81f4ed8e11fcbd0fc8ea2b
RIYA2410 <riyajainangel675@gmail.com>
2020-07-20 00:08:06 -0700

    my commit

--- customiseemail.py
+++ customiseemail.py
@@ -6,7 +6,7 @@ from pathlib import Path
 html=Template(Path('.\html\index.html').read_text())
 email=EmailMessage()
 email['from']='Riya Jain'
-email['to']='riyajainangel675@gmail.com'
+email['to']='riyajainangel5778@gmail.com'
 email['subject']='Python Programming'
 
 email.set_content(html.substitute(name = 'tintin'))
@@ -14,6 +14,6 @@ email.set_content(html.substitute(name = 'tintin'))
 with smtplib.SMTP(host='smtp.gmail.com',port=587) as smtp:
 	smtp.ehlo()
 	smtp.starttls()
-	smtp.login('riyajainangel675@gmail.com','jainriya2557')
+	smtp.login('riyajainangel675@gmail.com','password')
 	smtp.send_message(email)
 	print('email sent')
--- email_sender.py
+++ email_sender.py
@@ -3,7 +3,7 @@ from email.message import EmailMessage
 
 email=EmailMessage()
 email['from']='Riya Jain'
-email['to']='riya.tca1809040@tmu.ac.in'
+email['to']='riya.tca1804867@tmu.ac.in'
 email['subject']='Python Programming'
 
 email.set_content('I am learning Python')
@@ -11,6 +11,6 @@ email.set_content('I am learning Python')
 with smtplib.SMTP(host='smtp.gmail.com',port=587) as smtp:
 	smtp.ehlo()
 	smtp.starttls()
-	smtp.login('riyajainangel675@gmail.com','jainriya2557')
+	smtp.login('riyajainael675@gmail.com','password')
 	smtp.send_message(email)
 	print('email sent')
